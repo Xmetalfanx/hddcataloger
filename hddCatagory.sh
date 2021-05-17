cc0e93c29c434203462b106f98b66b4b0944af8a
Xmetalfanx <Xmetalfanx@yahoo.com>
2021-05-17 16:47:42 -0400

    working subdir checks in videos directory

--- hddCatagory.sh
+++ hddCatagory.sh
@@ -10,7 +10,7 @@ function catalogVideoFiles()
 {
     ## Checks for a movies and tvshows dir in the root of the ext. hdd being checked
     # things here should be in the root folder of the external hdd 
-    universalCheckForDirs "movies" "tvshows" "christmas_related" "tgc"
+    #universalCheckForDirs "movies" "tvshows" "christmas_related" "tgc"
     
     # run check for custom vids
     checkForVideoDir
